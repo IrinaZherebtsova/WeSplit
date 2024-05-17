5db81179c8f6e648cdfe61ae4fff092ca9470ba7
Irina Zherebtsova <izherebtsova@icloud.com>
2024-05-17 10:40:57 -1000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Irina Zherebtsova on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
